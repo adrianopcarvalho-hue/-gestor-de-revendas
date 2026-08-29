dcb3c3d18faa33b25f7fff27a8721ce5d164d43a
adrianopcarvalho-hue <adriano.pcarvalho@icloud.com>
2026-08-29 08:50:23 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
-const CACHE="gestor-revendas-v4";
+const CACHE="gestor-revendas-v5";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c=>c.addAll(ASSETS))
-      .then(()=>self.skipWaiting())
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
   );
 });
-
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
+    )).then(()=>self.clients.claim())
   );
 });
-
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
-      const y=x.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,y));
-      return x;
-    }).catch(()=>caches.match("./index.html")))
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      if(cached) return cached;
+      return fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        return response;
+      }).catch(()=>caches.match("./index.html"));
+    })
   );
 });
